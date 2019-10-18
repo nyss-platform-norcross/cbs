41109645c895dca16ccec3d4e8192d82f9f333b6
Ashraf <ashraf@post.com>
2019-10-18 16:18:07 +0200

    Added Sidebar to Layout component

--- src/Nyss.Web/ClientApp/src/components/Layout.js
+++ src/Nyss.Web/ClientApp/src/components/Layout.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import NavMenu from './NavMenu';
+import React from "react";
+import NavMenu from "./NavMenu";
+import SideBar from "./SideBar";
+import "../styles/screen.css";
 
 export default props => (
   <div>
     <NavMenu />
-    <Container>
+    <div className={"custom-container"} style={{ paddingLeft: "272px" }}>
+      <SideBar />
       {props.children}
-    </Container>
+    </div>
   </div>
 );

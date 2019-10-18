785203581afbd49edfd0ac12074a7c19ee0596fb
Ashraf <ashraf@post.com>
2019-10-18 16:19:17 +0200

    Prepared Projects component

---
+++ src/Nyss.Web/ClientApp/src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Projects = props => {
+  return (
+    <div>
+      <p>{"Projects"}</p>
+    </div>
+  );
+};
+
+export default connect()(Projects);

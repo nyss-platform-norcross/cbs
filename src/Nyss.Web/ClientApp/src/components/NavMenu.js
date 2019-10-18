a7b3c458748ba3092fb60ece99d74c4e66699013
mansan souleiman ahmed <mansan.souleiman.ahmed@gmail.com>
2019-10-18 16:11:59 +0200

    initial-push

--- src/Nyss.Web/ClientApp/src/components/NavMenu.js
+++ src/Nyss.Web/ClientApp/src/components/NavMenu.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import React from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavMenu.css";
 
 export default class NavMenu extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
@@ -12,17 +20,21 @@ export default class NavMenu extends React.Component {
       isOpen: false
     };
   }
-  toggle () {
+  toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
-  render () {
+  render() {
     return (
       <header>
-        <Navbar className="border-bottom box-shadow" light >
-            <Container>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/IFRC_Logo.png" alt="Red Cross logo" width="80px"/>
+        <Navbar className="border-bottom box-shadow" light>
+          <Container>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/c/c9/IFRC_Logo.png"
+              alt="Red Cross logo"
+              width="80px"
+            />
           </Container>
         </Navbar>
       </header>
---
+++ src/Nyss.Web/ClientApp/src/components/responsive/createData.js
@@ -0,0 +1,264 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+//Components
+import TabCreateDataComponent from "./TabCreateDataComponent";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 650
+  }
+});
+
+function createData(
+  time,
+  healthRisk,
+  status,
+  region,
+  district,
+  village,
+  dataCollector,
+  maleUnderFive,
+  femaleUnderFive,
+  maleOverFive,
+  femaleOverFive
+) {
+  return {
+    time,
+    healthRisk,
+    status,
+    region,
+    district,
+    village,
+    dataCollector,
+    maleUnderFive,
+    femaleUnderFive,
+    maleOverFive,
+    femaleOverFive
+  };
+}
+
+const rows = [
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  ),
+  createData(
+    "14.10.2019",
+    "Measles",
+    "Success",
+    "Nothern Province",
+    "Banbali",
+    "Sanda loko",
+    "Adama",
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    0
+  )
+];
+
+export default function SimpleTable() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>Report list</h1>
+      <TabCreateDataComponent />
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Time</TableCell>
+              <TableCell align="right">Health risk</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Region</TableCell>
+              <TableCell align="right">District</TableCell>
+              <TableCell align="right">Village</TableCell>
+              <TableCell align="right">Data collector</TableCell>
+              <TableCell align="right">Male under 5 </TableCell>
+              <TableCell align="right">Female under 5 </TableCell>
+              <TableCell align="right">Male over 5 </TableCell>
+              <TableCell align="right">Female over 5 </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.time}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.healthRisk}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.status}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.region}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.district}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.village}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.dataCollector}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.maleUnderFive}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.maleOverFive}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.femaleUnderFive}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.femaleOverFive}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  );
+}
